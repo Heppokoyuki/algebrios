b5df8e499e149075db48240f622eb921a88f1d1b
Heppokoyuki <yuki@uefi.jp>
2019-08-15 23:56:57 +0900

    refactoring paging initalization

--- src/paging.c
+++ src/paging.c
@@ -15,32 +15,33 @@ init_kernel_page_table(void)
     uint64_t *pt;
 
     base = KERNEL_PAGE_TABLE_BASE_ADDRESS;
-    pml4 = (uint64_t *)base;
 
+    /* PML4E */
+    pml4 = (uint64_t *)base;
     /* zero clear PML4E */
     for(int i = 0; i < 512; ++i)
-        *(pml4 + i) = 0;
+        pml4[i] = 0;
+    pml4[0] = (base + 0x1000) | 0x07;
 
-    *pml4 = (base + 0x1000) | 0x07;
-
+    /* PDPTE */
     pdpt = (uint64_t *)(base + 0x1000);
-
     /* zero clear PDPTE */
     for(uint64_t i = 0; i < 512; ++i)
-        *(pdpt + i) = 0;
-
+        pdpt[i] = 0;
     for(uint64_t i = 0; i < 4; ++i) {
-        *(pdpt + i) = (base + 0x2000 + 0x1000 * i) | 0x07;
+        pdpt[i] = (base + 0x2000 + 0x1000 * i) | 0x07;
     }
 
+    /* PDE */
     pd = (uint64_t *)(base + 0x2000);
     for(uint64_t i = 0; i < 512 * 4; ++i) {
-        *(pd + i) = (base + 0x6000 + 0x1000 * i) | 0x07;
+        pd[i] = (base + 0x6000 + 0x1000 * i) | 0x07;
     }
 
+    /* PTE */
     pt = (uint64_t *)(base + 0x6000);
     for(uint64_t i = 0; i < 512 * 512 * 4; ++i) {
-        *(pt + i) = (0x1000 * i) | 0x07;
+        pt[i] = (0x1000 * i) | 0x07;
     }
 
     set_cr3(base);
